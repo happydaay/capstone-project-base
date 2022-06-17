b4abd4db558480b0f2d406a41ff9e64f29796e88
happydaay <104245181+happydaay@users.noreply.github.com>
2022-06-17 14:06:06 +0900

    Add files via upload

---
+++ IncomeVerificationBC/src/main/java/newestaccount/domain/CustomerRegisteredE.java
@@ -0,0 +1,14 @@
+package newestaccount.domain;
+
+import java.util.Date;
+import lombok.Data;
+import newestaccount.domain.*;
+import newestaccount.infra.AbstractEvent;
+
+@Data
+public class CustomerRegisteredE extends AbstractEvent {
+
+    private Long id;
+    // keep
+
+}
